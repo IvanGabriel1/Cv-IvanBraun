5b0c852bee48cf9ab148ecfda8b7b33d3b1b2d2e
IvanGabriel1 <ivangabraun@gmail.com>
2025-12-21 20:06:36 -0300

    cambio de texto perfil profesional

--- src/components/Perfil-Profesional/PerfilProfesional.jsx
+++ src/components/Perfil-Profesional/PerfilProfesional.jsx
@@ -8,13 +8,12 @@ const PerfilProfesional = () => {
         <h3 className="perfil-title">Perfil Profesional</h3>
 
         <p className="perfil-text">
-          Desarrollador Front-End especializado en React y React Native, con
-          experiencia en el desarrollo de aplicaciones web y móviles utilizando
-          JavaScript, HTML, CSS y Firebase. Trabajo de forma autónoma, con
-          enfoque en resultados y buenas prácticas. Me especializo en crear
-          interfaces funcionales, escalables y orientadas a la experiencia de
-          usuario, acompañando proyectos desde la idea inicial hasta su
-          implementación.
+          Desarrollador Front-End freelance, especializado en React y React
+          Native. Experiencia en el desarrollo de aplicaciones web y móviles con
+          JavaScript, HTML, CSS y Firebase. Trabajo de forma autónoma, orientado
+          a resultados y buenas prácticas, acompañando proyectos desde la idea
+          inicial hasta su implementación, con foco en la experiencia de
+          usuario.
         </p>
 
         <div className="perfil-hardskills-wrapper">
